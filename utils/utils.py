943ddd9f0b5993a3b593cfb9e65e3dbdf8afb8e1
NhatAnh <nhatanh81096@yahoo.com>
2018-07-19 21:33:04 +0700

    - Add code to load images and some utils - Restructure folder - Add code to train model

--- utils/utils.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def split_train_val(dataset, val_percent=0.05):
-    dataset = list(dataset)
-    length = len(dataset)
-    n = int(length * val_percent)
-    random.shuffle(dataset)
-    return {'train': dataset[:-n], 'val': dataset[-n:]}
